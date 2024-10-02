074332bd7bfe6be1d6f302b3ec26644e2832245f
Alyona <olenapav97@gmail.com>
2024-10-02 21:17:04 +0300

    аа

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector('#controls'); 
+const input = controls.querySelector('input'); 
+const createButton = controls.querySelector('[data-create]'); 
+const destroyButton = controls.querySelector('[data-destroy]'); 
+const boxesContainer = document.querySelector('#boxes'); 
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+
+  destroyBoxes();
+  createBoxes(amount);
+  input.value = ``;
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+  inputvalue = ''
+});
+
+function createBoxes(amount) {
+  const boses = [];
+  for (let i = 0, i < amount; i++)
+    const size = 30 + i * 10;
+  
+}
